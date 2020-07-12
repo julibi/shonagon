470b156eaf4d2ff7a86703b26916aeb5c78f2663
hyun.yi <hyun.yi@xing.com>
2020-07-12 19:04:20 +0200

    fix bug, remove BE & unused files

--- db/mongoose.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const keys = require('../config/keys');
-
-mongoose.Promise = global.Promise;
-mongoose.connect(keys.mongoURI);
-
-module.exports = { mongoose };
