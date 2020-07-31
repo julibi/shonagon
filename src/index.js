efb70f1d0be950c1ce4be1150de3ab1b8f08f1b8
hyun.yi <hyun.yi@xing.com>
2020-07-31 15:24:22 +0200

    removes redux setup completely

--- src/index.js
+++ src/index.js
@@ -1,28 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import ReduxPromise from 'redux-promise';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import rootReducer from './reducers';
+
 import './index.css';
 import ReadSingleSnuContainer from './components/ReadSingleSnu/ReadSingleSnu';
 import AboutPage from './components/AboutPage';
-
 import registerServiceWorker from './registerServiceWorker';
 
-
-const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
-
 const App = () => (
-  <Provider store={createStoreWithMiddleware(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
     <Router>
       <Switch>
         <Route exact path="/" component={ReadSingleSnuContainer} />
         <Route exact path="/about" component={AboutPage} />
       </Switch>
     </Router>
-  </Provider>
 );
 
 render(<App/>, document.getElementById('root'));
--- src/selectors/index.js
+++
@@ -1,8 +0,0 @@
-import { createSelector } from 'reselect';
-
-// const readSnus = 
-//   state => state.snus.filter(snu => snu.read === true)
-// ;
-
-export { readSnus };
-
